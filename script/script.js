fda5a4a354e10d98daee0fb5ab7a8b2d2d004e4f
SJaniani <sergio.9949@aluno.pr.senac.br>
2025-10-20 17:29:14 -0300

    aula 20102025

---
+++ script/script.js
@@ -0,0 +1,107 @@
+$('#clientCPF').mask('000.000.000-00');
+$('#clientTelefone').mask('(00) 00000-0000');
+
+$('#clientSalario').maskMoney({
+    prefix: 'R$ ',
+    allowNegative: false, thousands: '.', decimal: ',', affixesStay: false
+});
+
+function validarCPF(cpf) {
+    cpf = cpf.replace(/[.-]/g, ""); // Remove pontos e traços
+    if (cpf.length !== 11 || /^(\\d)\\1{10}$/.test(cpf)) {
+        return false;
+    }
+    let soma = 0;
+    let resto;
+    for (let i = 1; i <= 9; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.substring(9, 10))) {
+        return false;
+    }
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.substring(10, 11))) {
+        return false;
+    }
+    return true;
+}
+
+document.getElementById("clientCPF").addEventListener("blur", function () {
+    const cpf = this.value;
+    if (!validarCPF(cpf)) {
+        alert("CPF inválido. Por favor, insira um CPF válido.");
+    }
+});
+
+document.getElementById('formCadastro').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('clientName').value;
+    const cpf = document.getElementById('clientCPF').value;
+    const telefone = document.getElementById('clientTelefone').value;
+    const dtNascimento = document.getElementById('clientDtNascimento').value;
+    const salario = document.getElementById('clientSalario').value;
+    console.log(salario);
+    const salarioConvertido = converterEmCentavos(salario);
+    console.log(salarioConvertido);
+    const credito = salarioConvertido * 0.3;
+    const creditoEmReal = converterEmReal(credito);
+
+    localStorage.setItem('name', name);
+    localStorage.setItem('cpf', cpf);
+    localStorage.setItem('telefone', telefone);
+    localStorage.setItem('dtNascimento', dtNascimento);
+    localStorage.setItem('salario', salario);
+    localStorage.setItem('creditoDisponivel', creditoEmReal);
+
+    limparFormulario();
+    alert('Cliente cadastrado com sucesso!');   
+});
+
+function limparFormulario() {
+    document.getElementById('clientName').value = '';
+    document.getElementById('clientCPF').value = ''; ;
+    document.getElementById('clientTelefone').value = '';
+    document.getElementById('clientDtNascimento').value = '';
+    document.getElementById('clientSalario').value = '';
+}
+
+function mostrarCliente() {
+    const name = localStorage.getItem('name');
+    const cpf = localStorage.getItem('cpf');
+    const telefone = localStorage.getItem('telefone');
+    const dtNascimento = localStorage.getItem('dtNascimento');
+    const salario = localStorage.getItem('salario');
+    const creditoDisponivel = localStorage.getItem('creditoDisponivel');
+
+    alert(
+        'Nome do cliente: ' + name + '\n' +
+        'CPF: ' + cpf + '\n' +
+        'Telefone: ' + telefone + '\n' +
+        'Data de Nascimento: ' + dtNascimento + '\n' +
+        'Salário: R$ ' + salario + '\n' +
+        'Crédito Disponível: R$ ' + creditoDisponivel
+    )};  
+
+
+
+function converterEmCentavos(salario) {
+    const salarioEmCentavos = salario.replace(/[^\d,]/g, '').replace(',', '.');
+    const salarioEmNumero = parseFloat(salarioEmCentavos);
+
+    return Math.round(salarioEmNumero * 100);
+}
+
+function converterEmReal(credito) {
+    const creditoConvertido = (credito / 100).toFixed(2);
+    return creditoConvertido.replace('.', ',')
+        .replace(/\d(?=(\d{3})+,)/g, '$&');
+
+}
